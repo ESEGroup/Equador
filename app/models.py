16e42b0e669989c3f5d07a982d76d89ce4d4af77
Thiago Luis Azevedo Maia <thiagoluismaia@poli.ufrj.br>
2016-12-18 23:23:40 -0200

    Algumas classes no Model

--- app/models.py
+++ app/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Status(models.Model):
+
+	id = models.AutoField(primary_key = True)
+	nome = models.CharField(max_length=200)
+
+
+class Equipamento(models.Model):
+
+	nome = models.CharField(max_length=200)
+	fabricante = models.CharField(max_length=200)
+	status = models.ForeignKey('Status', on_delete=models.CASCADE)
+
+class Manutencao(models.Model):
+
+	data_inicio = models.DateTimeField()
+	data_fim = models.DateTimeField()
+	equipamento = models.ForeignKey('Equipamento')
+	funcionario = models.ForeignKey('Fucionario')
+	
