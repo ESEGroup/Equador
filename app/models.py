1819516b3821e71fddea914f9d1aea924c47bafb
joaoceles <joaoceles@poli.ufrj.br>
2016-12-18 23:33:04 -0200

    Segue Versoes iniciais das Classes de Usuarios

--- app/models.py
+++ app/models.py
@@ -1,5 +1,42 @@
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.db import models
 
+from enum import Enum 
+
 # Create your models here.
+
+	
+
+class Usuario(models.Model):
+	nome = models.CharField(max_lenght=100)
+	cpf = models.IntegerField()
+	profissao = models.ForeignKey(choices=["AdmGeral","AdmDep", "FuncioMan"])
+
+	def selecionar():
+		return nome 
+		
+
+class Administrador(Usuario):
+	departamento = models.CharField ()
+
+
+	def 
+
+class Funcionario(Usuario):
+
+class RequisicaoManut():
+	codigoDeManutencao = models.CharField(max_lenght=10)
+
+
+
+	''''nome = models.CharField(max_lenght=30)
+	cpf = models.IntegerField()
+	profissao = models.ForeignKey(Profissao)
+'''
+
+'''	def mostradados():
+		print(nome)
+		print(cpf)
+		print(profissao)
+
